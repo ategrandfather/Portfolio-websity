aaa8bb828963414f38030e72e95c64315421a729
ategrandfather <mflentin@gmail.com>
2025-11-01 09:33:38 +0000

    Fix Postgres URL for SQLAlchemy

--- config.py
+++ config.py
@@ -1,21 +1,14 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     if not SECRET_KEY:
-        raise RuntimeError("SECRET_KEY is mising in the environment.")
+        raise RuntimeError("SECRET_KEY is missing in the environment.")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///app.db",
-    )
-    print(SQLALCHEMY_DATABASE_URI)
+    db_url = os.getenv("DATABASE_URL")
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql+psycopg2://")
+
+    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-aa = Config()
-print(aa)
